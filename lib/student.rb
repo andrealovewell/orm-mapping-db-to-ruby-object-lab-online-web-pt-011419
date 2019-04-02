d0f7933d9a4bc6c206f373e460be0e01e984881e
Andrea Lovewell <andy.lovewell@gmail.com>
2019-04-02 04:06:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ def self.count_all_students_in_grade_9
   sql = <<-SQL
   SELECT *
   FROM students
-  WHERE students.grade = 9
+  WHERE students.grade == 9
   SQL
   DB[:conn].execute(sql).collect do |row|
     self.new_from_db(row)
